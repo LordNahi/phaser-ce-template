c9b6096e3370c973932b2df86be1671c2b7f4402
Nathan Nahi <nathannahisteam@gmail.com>
2021-11-06 17:23:14 +1300

    Prettier

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
-import 'pixi';
-import 'p2';
-import Phaser, { IGameConfig } from 'phaser-ce'
+import "pixi"
+import "p2"
+import Phaser, { IGameConfig } from "phaser-ce"
 
 const config: IGameConfig = {
-  width: 800,
-  height: 600,
-  parent: "game",
-};
+    width: 800,
+    height: 600,
+    parent: "game",
+}
 
 class Game extends Phaser.Game {
-  constructor(config: IGameConfig) {
-    super(config);
-  }
+    constructor(config: IGameConfig) {
+        super(config)
+    }
 }
 
 window.onload = () => {
-  const game = new Game(config);
-};
+    const game = new Game(config)
+}
