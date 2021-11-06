069c22669ba1ec2c9cf8d67e8a7facdd5ed7a225
Nathan Nahi <nathannahisteam@gmail.com>
2021-11-06 17:37:08 +1300

    Format: Add semicolons

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
-import "pixi"
-import "p2"
-import Phaser, { IGameConfig } from "phaser-ce"
+import "pixi";
+import "p2";
+import Phaser, { IGameConfig } from "phaser-ce";
 
 const config: IGameConfig = {
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 768,
     parent: "game",
-}
+};
 
 class Game extends Phaser.Game {
     constructor(config: IGameConfig) {
-        super(config)
+        super(config);
     }
 }
 
 window.onload = () => {
-    const game = new Game(config)
-}
+    const game = new Game(config);
+};
