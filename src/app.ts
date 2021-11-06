e84c44952e898179c29c9c08b8eb0b7d037e61c9
Nathan Nahi <nathannahisteam@gmail.com>
2021-11-06 18:40:04 +1300

    Refactor app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,28 @@ import "pixi";
 import "p2";
 import Phaser, { IGameConfig } from "phaser-ce";
 
-const config: IGameConfig = {
+import LoadingScreen from "./Screens/loading";
+
+const defaultConfig: IGameConfig = {
     width: 1024,
     height: 768,
     parent: "game",
+    renderer: Phaser.AUTO,
 };
 
 class Game extends Phaser.Game {
     constructor(config: IGameConfig) {
         super(config);
+
+        // Prepare Loading Stack ...
+        this.state.add("loading", LoadingScreen);
+        this.state.add("game", LoadingScreen);
+
+        // Kick off loader ...
+        this.state.start("loading");
     }
 }
 
 window.onload = () => {
-    const game = new Game(config);
+    new Game(defaultConfig);
 };
